853d6e934181f5a5d6186d3cf47e38b172f2b13f
Rizki Pratama <rizkipratama0602@gmail.com>
2021-12-06 00:22:41 +0700

    day 5

---
+++ day5/HydrothermalVenture.py
@@ -0,0 +1,15 @@
+import numpy as np
+from skimage.draw import line
+
+INPUT_FILE = "input.txt"
+
+with open(INPUT_FILE, "r") as f:
+    coordinates = [[[int(k) for k in c.split(",")] for c in x.strip().split(" -> ")] for x in f]
+
+overlappingLines = np.zeros((1000,1000), dtype=np.uint8)
+
+for ((x1, y1), (x2, y2)) in coordinates:
+    rr, cc = line(x1, y1, x2, y2)
+    if x1 == x2 or y1 == y2:
+        overlappingLines[rr, cc] += 1
+print(len(overlappingLines[overlappingLines > 1]))
