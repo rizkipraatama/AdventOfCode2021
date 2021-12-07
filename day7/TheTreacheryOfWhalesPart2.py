208082d9f1075b5a494782b7f1a81e83b65aafa0
Rizki Pratama <rizkipratama0602@gmail.com>
2021-12-07 15:21:24 +0700

    day 7

---
+++ day7/TheTreacheryOfWhalesPart2.py
@@ -0,0 +1,21 @@
+INPUT_FILE = "input.txt"
+
+with open(INPUT_FILE, "r") as f:
+    input = [x.split(",") for x in f]
+    horizontalPosition = [int(x) for x in input[0]]
+
+minimumPosition = min(horizontalPosition)
+maximumPosition = max(horizontalPosition)
+
+minimumFuel = float("inf")
+
+for i in range(minimumPosition, maximumPosition):
+    fuel = 0
+    for j in horizontalPosition:
+        usedFuel = abs(j - i)
+        for k in range(1, usedFuel+1):
+            fuel += k
+    if fuel < minimumFuel:
+        minimumFuel = fuel
+
+print(minimumFuel)
