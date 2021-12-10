5eb29b5a06219af64b771e59d07958b04bb3d505
Rizki Pratama <rizkipratama0602@gmail.com>
2021-12-11 00:37:02 +0700

    rename variables

--- day9/SmokeBasin.py
+++ day9/SmokeBasin.py
@@ -11,10 +11,10 @@ width = len(heightmap[0])
 def neighbors(j, i):
     return set([(abs(j-1), i), (j, abs(i-1)), (j+1 - 2*(j==(height-1)), i), (j, i+1 - 2*(i==(width-1)))])
 
-r1 = 0
+riskLevel = 0
 for j in range(height):
     for i in range(width):
         if all([heightmap[j][i] < heightmap[y][x] for y, x in neighbors(j, i)]):
-            r1 += 1 + heightmap[j][i]
+            riskLevel += 1 + heightmap[j][i]
 
-print(r1)
+print(riskLevel)
